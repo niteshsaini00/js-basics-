4daef2294ba7589d068e6f822550a5c95aa3306f
niteshsaini00 <168090094+niteshsaini00@users.noreply.github.com>
2024-05-08 13:14:26 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+// variables in js 
+
+
+// let a = 2
+// console.log(a);
+
+// var a = "nitesh"
+// console.log(a);
+
+// const a = "mohan"
+
+// console.log(a);
+
+// primitives and objects in js
+
+
+// let a = null;
+// let b = 1245463;
+// let c = "nitesh";
+// let d = Symbol("this is a symbol");
+// let e = BigInt("12")+BigInt("8");
+// let f = true ;
+// console.log(a,b,c,d,e,f);
+
+
+// operaters and expressions in js 
+
+
+// operaters in js 
+
+
+// +  addition 
+// -  subtraction
+// *  multiplication
+// ** exponention
+// /  division
+// %  modulas 
+// ++ increment 
+// -- decrement 
+
+// examples
+ 
+let a = 10;
+let b = 20;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a**b);
+// console.log(a/b);
+// console.log(a%b);
+
+
+// assiment operaters 
+
+
+// let comp1 = 6;
+// let comp2 = 7;
+// console.log("comp1==comp2 is",comp1==comp2)
+
+
+let x = 5;
+let y = 6;
+
+console.log(x<y && x==5);
